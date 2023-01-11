94a5f97961b54d520caad9be629d5a9fb17300d9
tk <trishank01@gmail.com>
2023-01-11 21:34:34 +0530

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,10 +12,18 @@ const Home = () => {
     
   }
   return (
+    <div>
+        <head>
+        <meta
+      name="description"
+      content="Home page"
+    />
+        </head>
     <div className='flex flex-col h-screen bg-color-base text-white justify-center items-center gap-6'>
         <h1>Home</h1>
         <h3>Welcome {user.name}</h3>
         <button className='border-none p-3 font-semibold rounded-lg bg-color-brand cursor-pointer' onClick={handleLogout}>LogOut</button>
+    </div>
     </div>
   )
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,10 +5,70 @@ import { MdOutlineLightMode } from "react-icons/md";
 import Captcha from "../components/captcha/Captcha";
 import { useNavigate } from "react-router-dom";
 
-let allCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-                     'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
-                     'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-                     't', 'u', 'v', 'w', 'x', 'y', 'z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let allCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+];
 
 const Login = () => {
   const [name, setName] = useState("");
@@ -18,10 +78,7 @@ const Login = () => {
   const [formValue, setformValue] = useState(
     JSON.parse(localStorage.getItem("user")) || {}
   );
-  const navigate = useNavigate()
-
-
-
+  const navigate = useNavigate();
 
   const { handleThemeSwitch, theme } = useContext(ThemeContext);
 
@@ -35,74 +92,86 @@ const Login = () => {
     setformValue(getFormValue);
     localStorage.setItem("user", JSON.stringify(getFormValue));
 
-    if(formValue){
-      navigate("/home")
+    if (formValue) {
+      navigate("/home");
     }
   };
 
-  console.log(formValue)
+  console.log(formValue);
 
   return (
-    <section
-      className={`flex h-screen justify-center items-center bg-[#F2F2F2] dark:bg-color-base`}
-    >
-      <div>
-        <h2 className="text-[24px] text-color-base  dark:text-color-brand flex justify-center">
-          Register Your Self
-        </h2>
-        <div className="w-[370px] md:w-[400px] h-[500px] mt-7">
-          <form
-            onSubmit={handleFormSubmit}
-        
-            className="dark:bg-white bg-[#c2ced8] p-5 rounded-xl drop-shadow-lg"
-          >
-            <div className="m-8 ">
-              <input
-                className="p-4 w-[100%] bg-[#F2F2F2] rounded-lg text-grey font-semibold border-0 focus:outline-none"
-                type="text"
-                placeholder="Your Name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+    <div>
+      <head>
+        <meta name="description" content="Home page" />
+      </head>
+
+      <section
+        className={`flex h-screen justify-center items-center bg-[#F2F2F2] dark:bg-color-base`}
+      >
+        <div>
+          <h2 className="text-[24px] text-color-base  dark:text-color-brand flex justify-center">
+            Register Your Self
+          </h2>
+          <div className="w-[370px] md:w-[400px] h-[500px] mt-7">
+            <form
+              onSubmit={handleFormSubmit}
+              className="dark:bg-white bg-[#c2ced8] p-5 rounded-xl drop-shadow-lg"
+            >
+              <div className="m-8 ">
+                <input
+                  className="p-4 w-[100%] bg-[#F2F2F2] rounded-lg text-grey font-semibold border-0 focus:outline-none"
+                  type="text"
+                  placeholder="Your Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="m-8">
+                <input
+                  className="p-4 w-[100%] bg-[#F2F2F2] rounded-lg text-grey font-semibold font-sans border-0 focus:outline-none"
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="m-8">
+                <input
+                  className="p-4 w-[100%] bg-[#F2F2F2] rounded-lg text-grey font-semibold font-sans border-0 focus:outline-none"
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <Captcha
+                setIsCaptchaValid={setIsCaptchaValid}
+                isCaptchaValid={isCaptchaValid}
               />
-            </div>
-            <div className="m-8">
-              <input
-                className="p-4 w-[100%] bg-[#F2F2F2] rounded-lg text-grey font-semibold font-sans border-0 focus:outline-none"
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="m-8">
-              <input
-                className="p-4 w-[100%] bg-[#F2F2F2] rounded-lg text-grey font-semibold font-sans border-0 focus:outline-none"
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <Captcha setIsCaptchaValid={setIsCaptchaValid} isCaptchaValid={isCaptchaValid}/>
-            <div className="m-8">
-              <button type="submit" disabled={isCaptchaValid === false} className="p-4 w-[100%] cursor-pointer text-white disabled:bg-gray-400  bg-color-base font-bold rounded-lg border-0 focus:outline-none  disabled:cursor-not-allowed">
-                Sign In
-              </button>
-            </div>
-          </form>
-          <button
-            onClick={handleThemeSwitch}
-            className="fixed top-5 right-5 bg-gray-400 border-0 rounded-lg p-2 cursor-pointer"
-          >
-            {theme === "dark" ? (
-              <MdOutlineLightMode size={28} />
-            ) : (
-              <MdDarkMode size={28} />
-            )}
-          </button>
+              <div className="m-8">
+                <button
+                  type="submit"
+                  disabled={isCaptchaValid === false}
+                  className="p-4 w-[100%] cursor-pointer text-white disabled:bg-gray-400  bg-color-base font-bold rounded-lg border-0 focus:outline-none  disabled:cursor-not-allowed"
+                >
+                  Sign In
+                </button>
+              </div>
+            </form>
+            <button
+              onClick={handleThemeSwitch}
+              className="fixed top-5 right-5 bg-gray-400 border-0 rounded-lg p-2 cursor-pointer"
+            >
+              {theme === "dark" ? (
+                <MdOutlineLightMode size={28} />
+              ) : (
+                <MdDarkMode size={28} />
+              )}
+            </button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
